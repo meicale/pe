c69b88a3f5e0865835c4f7565a5d0d800888d7c0
meicale <bill@DESKTOP-M0LLBM9.>
2024-07-20 07:31:32 +0800

    merge remote dev branch

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -49,4 +49,24 @@ return {
       })
     end,
   },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = "leetcode.nvim" ~= vim.fn.argv()[1],
+    opts = {
+      -- configuration goes here
+      arg = "leetcode.nvim",
+      lang = "python3",
+    },
+  },
 }
